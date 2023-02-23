647204791edb0bad132bcd4b737a0012af69dd12
Ivan Buetler <ivan.buetler@compass-security.com>
2023-02-23 07:39:28 +0100

    update

--- Readme.md
+++ Readme.md
@@ -1,5 +1,13 @@
 # Plot HL usage
+## Preparation
+```
+apt install python3.10-venv
+python3 -m venv .
+. ./bin/activate
+python -m pip install matplotlib dash pandas
+```
 
+## Usage
 1. copy `docker-auslastung.log` from vm-docker-01 to this folder
 2. run `prepare.sh` and this will create the cvs output file `source-log`
 
--- plot-stats.py
+++ plot-stats.py
@@ -3,7 +3,6 @@ import pandas as pd
 import plotly.express as px
 import argparse, os
 
-<<<<<<< HEAD
 parser = argparse.ArgumentParser()
 parser.add_argument("-f", "--file", help="path to the source log file", required=True)
 args = parser.parse_args()
@@ -15,9 +14,6 @@ if not os.path.exists(args.file):
 
 
 df = pd.read_csv(args.file)
-=======
-df = pd.read_csv("trend.log")
->>>>>>> ad5b1f8c045c9339c71f6423f9140eb486bee029
 
 fig = px.line(df, x = 'date', y = 'usage', title='Vulnerable Docker Status', render_mode='svg')
 fig.show()
